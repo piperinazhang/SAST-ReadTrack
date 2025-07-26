92847d9a68d53f2eb771ac662314336e8abf91df
Piperina <b24042003@njupt.edu.cn>
2025-07-26 11:49:30 +0800

    匹配sql，新增impl

---
+++ src/main/java/com/sast/sastreadtrack/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.sast.sastreadtrack.service.impl;
+
+import com.sast.sastreadtrack.entity.User;
+import com.sast.sastreadtrack.mapper.UserMapper;
+import com.sast.sastreadtrack.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+
+/**
+ * 用户服务实现类
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    @Transactional
+    public boolean register(User user) {
+        //用户注册
+    }
+
+    @Override
+    public User login(String studentId, String password) {
+        //用户登录
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        //根据id获取用户信息
+    }
+}
